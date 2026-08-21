9a112147b75f13d922d2390b517977bd3af2df76
saulohdc89 <saulohdc1989@gmail.com>
2026-08-21 06:17:57 -0300

    feat: liga menu e cards às rotas com routerLink

--- src/app/big-card/big-card.ts
+++ src/app/big-card/big-card.ts
@@ -1,12 +1,14 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-big-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './big-card.html',
   styleUrl: './big-card.css'
 })
 export class BigCard {
+  id = input.required<string>();
   category = input.required<string>();
   title = input.required<string>();
   description = input.required<string>();
--- src/app/small-card/small-card.ts
+++ src/app/small-card/small-card.ts
@@ -1,12 +1,14 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-small-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './small-card.html',
   styleUrl: './small-card.css'
 })
 export class SmallCard {
+  id = input.required<string>();
   category = input.required<string>();
   title = input.required<string>();
   author = input.required<string>();
